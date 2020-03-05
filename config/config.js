abc37e51fdb6d35d3aa46f148febe48e2616dfe5
skykimpro <kjaa34@gmail.com>
2020-03-05 12:19:16 +0900

    Config file changed

--- config/config.js
+++ config/config.js
@@ -1,23 +1,26 @@
+require('dotenv').config();
+
+module.exports =
 {
   "development": {
     "username": "root",
-    "password": "1111",
+    "password": process.env.SEQUELIZE_PASSWORD,
     "database": "nodebird",
     "host": "localhost",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
-    "password": null,
+    "password": process.env.SEQUELIZE_PASSWORD,
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
-    "password": null,
+    "password": process.env.SEQUELIZE_PASSWORD,
     "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
-}
+};
