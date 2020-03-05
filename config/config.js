86af6c6c426dc5291a10948bb2f5b0f98400a7ec
skykimpro <kjaa34@gmail.com>
2020-03-05 12:31:43 +0900

    Config changed(to production)

--- config/config.js
+++ config/config.js
@@ -12,14 +12,14 @@ module.exports =
   "test": {
     "username": "root",
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "database_test",
+    "database": "nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": process.env.SEQUELIZE_PASSWORD,
-    "database": "database_production",
+    "database": "nodebird",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
